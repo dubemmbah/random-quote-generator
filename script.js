4ae767862685efff2f20d6cafce9fde27df08703
Chukwudubem Mbah <121401843+dubemmbah@users.noreply.github.com>
2023-12-25 22:38:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const heading = document.querySelector(".heading");
   const adviceElement = document.querySelector(".advice");
   const dice = document.querySelector(".dice");
+  const spinner = document.querySelector(".spinner-wrapper");
+  const adviceNumber = document.querySelector(".heading span");
+
+  let number = 0;
 
   //   Function to generate a random quote
   const fetchRandomQuote = async function () {
+    spinner.classList.remove("hidden");
     try {
       const response = await fetch("https://api.quotable.io/random");
       const data = await response.json();
@@ -14,16 +20,30 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log("Error fetching quote:", error);
     }
   };
-  fetchRandomQuote();
 
   // Function to update the advice text
   const updateAdvice = async () => {
+    number++;
+    resetAdvice();
+    adviceNumber.textContent = number;
     const randomQuote = await fetchRandomQuote();
     adviceElement.textContent = `"${randomQuote}"`;
+    heading.textContent = `Advice #${number}`;
+    spinner.classList.add("hidden");
   };
 
   //Event listener for the dice button click
-  dice.addEventListener("click", updateAdvice);
+  dice.addEventListener("click", function () {
+    resetAdvice();
+    setTimeout(updateAdvice, 2000);
+  });
+
+  //   Reset Advice
+  const resetAdvice = function () {
+    spinner.classList.remove("hidden");
+    heading.innerHTML = "";
+    adviceElement.textContent = "";
+  };
 
   //   Initial advice fetch on page load
   updateAdvice();
